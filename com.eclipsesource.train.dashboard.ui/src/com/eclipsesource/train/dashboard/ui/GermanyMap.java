d3365ab9fc77510efb420d7170ccda588d585c8a
Johannes Eickhold <jeick@eclipsesource.com>
2012-05-02 19:23:47 +0200

    Fixed layout of dialogs on Android. Please test on iOS!
    Fixed default data location for better platform independence.

--- com.eclipsesource.train.dashboard.ui/src/com/eclipsesource/train/dashboard/ui/GermanyMap.java
+++ com.eclipsesource.train.dashboard.ui/src/com/eclipsesource/train/dashboard/ui/GermanyMap.java
@@ -32,18 +32,17 @@ import com.eclipsesource.train.dashboard.model.Station;
 
 public class GermanyMap extends Composite {
 
-  private MarkerPool markerPool = new MarkerPool();
+  private final MarkerPool markerPool = new MarkerPool();
   private RailwayInfo currentInfo;
   private static final Image markerImage;
   private static final ConcurrentHashMap<ScaledImageKey, Image> scaledImages;
   private static final ImageData gemranyMapData;
-  
   static {
-    markerImage = Graphics.getImage( "/images/marker_white.png", GermanyMap.class.getClassLoader() ); 
+    markerImage = Graphics.getImage( "/images/marker_white.png", GermanyMap.class.getClassLoader() );
     gemranyMapData = new ImageData( GermanyMap.class.getResourceAsStream( "/images/germany.png" ) );
     scaledImages = new ConcurrentHashMap<ScaledImageKey, Image>();
   }
-  
+
   public GermanyMap( Composite parent, int style ) {
     super( parent, style );
     createBackground();
@@ -52,6 +51,7 @@ public class GermanyMap extends Composite {
   private void createBackground() {
     setBGImage();
     this.addListener( SWT.Resize, new Listener() {
+
       public void handleEvent( Event e ) {
         setBGImage();
       }
@@ -79,7 +79,8 @@ public class GermanyMap extends Composite {
 
   public void setInfo( RailwayInfo info ) {
     this.currentInfo = info;
-    List<Station> stationsSortedByDelayMinutes = info.getDelayInfo().getStationsSortedByDelayMinutes( 15 );
+    List<Station> stationsSortedByDelayMinutes = info.getDelayInfo()
+      .getStationsSortedByDelayMinutes( 15 );
     for( Station station : stationsSortedByDelayMinutes ) {
       addMarker( station.getLat(), station.getLon(), station );
     }
@@ -87,17 +88,22 @@ public class GermanyMap extends Composite {
   }
 
   public void addMarker( double longitude, double latitude, final Station station ) {
-    Point location = CoordinatesUtil.transformToPoint( new Coordinates( longitude, latitude ), this.getSize().x, this.getSize().y, new Coordinates( 54.8, 6.0 ), new Coordinates( 47.3, 14.7 ) );
+    Point location = CoordinatesUtil.transformToPoint( new Coordinates( longitude, latitude ),
+                                                       this.getSize().x,
+                                                       this.getSize().y,
+                                                       new Coordinates( 54.8, 6.0 ),
+                                                       new Coordinates( 47.3, 14.7 ) );
     Label marker = markerPool.getMarker();
     if( marker == null ) {
-      marker = new Label( this, SWT.NONE);
+      marker = new Label( this, SWT.NONE );
       marker.setData( WidgetUtil.CUSTOM_VARIANT, "ANIMATED" );
       markerPool.addMarker( marker );
     }
     marker.setImage( markerImage );
-    marker.setLocation( Math.max( 0, location.x -8), Math.max( 0, location.y -30 ) );
+    marker.setLocation( Math.max( 0, location.x - 8 ), Math.max( 0, location.y - 30 ) );
     marker.setSize( 32, 32 );
     marker.addListener( SWT.MouseDown, new Listener() {
+
       public void handleEvent( Event event ) {
         StringBuilder message = new StringBuilder();
         DelayInfo delayInfo = currentInfo.getDelayInfo();
@@ -106,30 +112,37 @@ public class GermanyMap extends Composite {
         int delayMinutesForStation = delayInfo.getDelayMinutesForStation( stationId );
         double delayPercentageForStation = delayInfo.getDelayPercentageForStation( stationId );
         message.append( "Delayed Trains: " );
-        message.append( String.valueOf( Math.round( delayAmountForStation) ) );
+        message.append( String.valueOf( Math.round( delayAmountForStation ) ) );
         message.append( " (" );
-        message.append( String.valueOf( Math.round( delayPercentageForStation) ) );
+        message.append( String.valueOf( Math.round( delayPercentageForStation ) ) );
         message.append( "%)\n" );
         message.append( "Total Minutes Delayed: " );
-        message.append( String.valueOf( Math.round( delayMinutesForStation) ) );
-        Shell dialog = new Shell( getDisplay(), SWT.CLOSE | SWT.BORDER | SWT.TITLE | SWT.APPLICATION_MODAL );
-        int width = 280;
-        int height = 130;
-        org.eclipse.swt.graphics.Point touchPoint = getDisplay().map( (Control)event.widget, null, event.x, event.y );
-        int x = Math.max( 1, Math.min( touchPoint.x, getDisplay().getBounds().width -width -1) );
-        int y = Math.max( 1, touchPoint.y -128);
-        dialog.setBounds( x, y, width, height );
-        dialog.setBackground( getDisplay().getSystemColor( SWT.COLOR_BLACK ) );
+        message.append( String.valueOf( Math.round( delayMinutesForStation ) ) );
+        Shell dialog = new Shell( getDisplay(), SWT.CLOSE
+                                                | SWT.BORDER
+                                                | SWT.TITLE
+                                                | SWT.APPLICATION_MODAL );
+        org.eclipse.swt.graphics.Point touchPoint = getDisplay().map( ( Control )event.widget,
+                                                                      null,
+                                                                      event.x,
+                                                                      event.y );
+        dialog.setBackground( getDisplay().getSystemColor( SWT.COLOR_GRAY ) );
         dialog.setText( station.getName() );
         dialog.setLayout( new FillLayout() );
-        Label messageLabel = new Label(dialog, SWT.WRAP );
-        messageLabel.setText( message.toString() );
-        messageLabel.setForeground( getDisplay().getSystemColor( SWT.COLOR_WHITE ) );
+        Label messageLabel = new Label( dialog, SWT.WRAP );
+        String messageText = message.toString();
+        messageLabel.setText( messageText );
+        messageLabel.setForeground( getDisplay().getSystemColor( SWT.COLOR_BLACK ) );
+        int width = dialog.getBounds().width;
+        int height = dialog.getBounds().height;
+        int x = Math.max( 1, Math.min( touchPoint.x, getDisplay().getBounds().width - width - 1 ) );
+        int y = Math.max( 1, touchPoint.y - 128 );
+        dialog.setBounds( x, y, width, height );
         dialog.open();
       }
     } );
   }
-  
+
   @Override
   public void dispose() {
     markerPool.dispose();
